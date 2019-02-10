06ad2bc1b91802b99229dabbcc35913f9ac7b292
Thais <Thais@THAIS-PC.local>
2019-02-10 19:04:00 -0200

    CRUD de Fazenda, proprietário e funcionário. Inicio da implementação do login com autenticação (com erro).

--- src/main/java/com/thais/demotcc/config/Autenticar.java
+++ src/main/java/com/thais/demotcc/config/Autenticar.java
@@ -1,14 +1,10 @@
 
 package com.thais.demotcc.config;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import java.security.Key;
-
 /**
  *
  * @author Thais
  */
 public class Autenticar {
-    public static final Key KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    
 }
